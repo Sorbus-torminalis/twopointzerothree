cf2bbd4be87c5ceb0d8418b7b8012e51c80d3f7c
Sorbus <Sorbus.torminalis@protonmail.com>
2020-09-15 17:22:05 +0200

    details

--- R/segment_cor.R
+++ R/segment_cor.R
@@ -1,6 +1,7 @@
 #' segment_cor
 #' @param vec numeric vector to test for possible offset duplicate sequences
 #' @param n length of duplicate sequence to search for
+#' @details Embeds the vector into low-dimensional Euclidean space and then finds correlation between all columns. Pairs of columns with a correlation of 1 are either duplicates or have an offset. 
 #' @examples
 #' set.seed(42) 
 #' x <- rnorm(10)
