e9e5cdda75c07526eb44a589ba98c8bdb4ab0bc0
Sorbus <Sorbus.torminalis@protonmail.com>
2020-09-15 17:22:18 +0200

    first tests

--- tests/testthat/test-segment_cor.R
+++ tests/testthat/test-segment_cor.R
@@ -1,3 +1,15 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("finds duplicates", {
+  set.seed(42) 
+  x <- rnorm(10)
+
+  s1 <- segment_cor(vec = x, n = 4) 
+  
+  #no duplicate
+  expect_lt(max(s1, na.rm = TRUE), 1)
+  
+  #duplicate sequence
+  x2 <- c(x, x[1:4] + 2.03)#with offset duplicate
+  s2 <- segment_cor(vec = x2, n = 4) 
+  expect_equal(max(s2, na.rm = TRUE), 1)
+  
 })
