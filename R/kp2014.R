dff01e6cc4c53623100ba45e14cc35a70beeaeb5
Sorbus <Sorbus.torminalis@protonmail.com>
2020-09-17 20:34:01 +0200

    adds real data

---
+++ R/kp2014.R
@@ -0,0 +1,21 @@
+#' Spider aggression data
+#'
+#' Spider aggression data for Keiser and Pruitt 2014 - Behavioral Ecology.
+#'
+#' @docType data
+#'
+#' @usage data(kp2014)
+#'
+#' @format A \code{"tibble"}.
+#'
+#' @keywords datasets
+#'
+#' @references Keiser, C. N., Pruitt, J.N. Spider aggressiveness determines the bidirectional consequences of host–inquiline interactions, _Behavioral Ecology_, 25: 142–151 2014, 
+#' (\href{http://dx.doi.org/10.1093/beheco/art096}{https://doi.org/10.1093/beheco/art096})
+#'
+#' @source \href{https://figshare.com/articles/Data_for_Keiser_and_Pruitt_2014_-_Behavioral_Ecology/11778552}{figshare}
+#'
+#' @examples
+#' data(kp2014)
+#' sequence_find(vec = kp2014$`Theridion murarium Aggressiveness...4`, max = 9)
+"kp2014"
---
+++ data-raw/kp2014.R
@@ -0,0 +1,16 @@
+## code to prepare `DATASET` dataset goes here
+#Data from Keiser & Pruitt (2014) https://figshare.com/articles/Data_for_Keiser_and_Pruitt_2014_-_Behavioral_Ecology/11778552
+
+library(tidyverse)
+
+if(!fs::file_exists("data-raw/kp2014.xlsx")){
+  download.file(
+    url = "https://ndownloader.figshare.com/files/21495579", 
+    destfile = "data-raw/kp2014.xlsx")
+}
+
+kp2014 <- readxl::read_excel(path = "data-raw/kp2014.xlsx", sheet = 1) %>% 
+  select(-matches("^Attack \\d+$")) %>% 
+  mutate(across(-c(`Singleton/Pair`, `Behavioral Types`, Treatment), as.numeric))
+
+usethis::use_data(kp2014, overwrite = TRUE)
