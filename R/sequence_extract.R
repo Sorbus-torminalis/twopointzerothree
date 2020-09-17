70676a74f8046c06317eadd578457e5257c2575b
Sorbus <Sorbus.torminalis@protonmail.com>
2020-09-17 18:16:45 +0200

    sequence_extrct

---
+++ R/sequence_extract.R
@@ -0,0 +1,26 @@
+#' extract duplicate sequences
+#' @param th result of sequence_thresh
+#' @param vec vector with possible duplicates
+#' @importFrom plyr adply
+#' @importFrom tibble tibble
+#' @examples 
+#' #' set.seed(42) 
+#' x <- rnorm(10)
+#' x <- c(x, x[1:4] + 2.03)
+#' co <- sequence_cor(vec = x, n = 3) 
+#' th <- sequence_thresh(co)
+#' sequence_extract(th, vec = x)
+#' @export
+
+sequence_extract <- function(th, vec){
+  dups <- which(th, arr.ind = TRUE)
+  dups <- dups[dups[, "row"] < dups[, "col"], , drop = FALSE]
+  n <- attr(th, "len")
+  plyr::adply(dups, 1, function(pos){
+    tibble(pos1 = pos[1]:(pos[1] + n - 1),
+           vec1 = vec[pos1],
+           pos2 = pos[2]:(pos[2] + n - 1),
+           vec2 = vec[pos2], 
+           delta = vec2 - vec2)
+  }, .id = "duplicate_no") 
+}
